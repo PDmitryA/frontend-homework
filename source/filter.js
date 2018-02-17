3d3c493c89c41981076b1c684d80876b57dcdd4e
PDmitryA <mitia.poponkin@yandex.ru>
2018-02-17 17:14:58 +0200

    simplified
    well-formatted, without bind

--- source/filter.js
+++ source/filter.js
@@ -8,18 +8,17 @@ var symbols = {
     "'": '&#39;',
 };
 
-function replacer(validTags, str) {
-    if(symbols[str]) return symbols[str];
-
-    for(let value of validTags) {
-        if(str.indexOf(value) > 0) return str;
-    }
-
-    let rep = replacer.bind(null, validTags);
-    return str.replace(/[<>"']/g, rep);
-}
-
 const filter = function(input, validTags){
-    let rep = replacer.bind(null, validTags);
+    function rep(str) {
+        if(symbols[str]) {
+        	return symbols[str];
+        }
+
+        for(let value of validTags) {
+        	if(str.indexOf(value) > 0) return str;
+        }
+
+        return str.replace(/[<>"']/g, rep);
+    }
     return input.replace(/<[^>]+>|["'<>&]/g, rep);
 }
